5b62b14f2c5d9daaf82628e27c0f5ce0305218d4
Volodymyr Shvaiko <cub4lt@gmail.com>
2021-12-01 17:16:06 +0200

    add action

--- ci/prepare-release.sh
+++ ci/prepare-release.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Setup git
-git config --global user.email "deploy@travis-ci.org"
-git config --global user.name "Deployment Bot"
-git config --global credential.helper "store"
-echo "https://${GITHUB_TOKEN}:@github.com" > $HOME/.git-credentials
-git checkout master
+# git config --global user.email "deploy@travis-ci.org"
+# git config --global user.name "Deployment Bot"
+# git config --global credential.helper "store"
+# echo "https://${GITHUB_TOKEN}:@github.com" > $HOME/.git-credentials
+# git checkout master
 
 # Setup conventional-github-releaser credentials
-export CONVENTIONAL_GITHUB_RELEASER_TOKEN="$GITHUB_TOKEN"
+# export CONVENTIONAL_GITHUB_RELEASER_TOKEN="$GITHUB_TOKEN"
 
 # Setup npm credentials
 echo ${NPM_TOKEN}
