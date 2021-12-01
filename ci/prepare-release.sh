9fd4eb3e1f4e0832971ce681e3c5b22496f3159a
Volodymyr Shvaiko <cub4lt@gmail.com>
2021-12-01 14:25:04 +0200

    add action

--- ci/prepare-release.sh
+++ ci/prepare-release.sh
@@ -4,11 +4,11 @@
 git config --global user.email "deploy@travis-ci.org"
 git config --global user.name "Deployment Bot"
 git config --global credential.helper "store"
-echo "https://${GITHUB_TOKEN}:@github.com" > $HOME/.git-credentials
-git checkout $TRAVIS_BRANCH
+#echo "https://${GITHUB_TOKEN}:@github.com" > $HOME/.git-credentials
+git checkout master
 
 # Setup conventional-github-releaser credentials
-export CONVENTIONAL_GITHUB_RELEASER_TOKEN="$GITHUB_TOKEN"
+#export CONVENTIONAL_GITHUB_RELEASER_TOKEN="$GITHUB_TOKEN"
 
 # Setup npm credentials
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > $HOME/.npmrc
+#echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > $HOME/.npmrc
