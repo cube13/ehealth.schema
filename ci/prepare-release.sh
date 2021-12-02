2eb01e2967a542851c89972cc1672b015943f02a
Volodymyr Shvaiko <cub4lt@gmail.com>
2021-12-02 16:08:01 +0200

    fix: fix vars

--- ci/prepare-release.sh
+++ ci/prepare-release.sh
@@ -3,12 +3,13 @@
 # Setup git
 git config --global user.email "cub4lt@gmail.com"
 git config --global user.name "Volodymyr Shvaiko"
-git config --global credential.helper "store"
-# echo "https://${GITHUB_TOKEN}:@github.com" > $HOME/.git-credentials
+#git config --global credential.helper "store"
+echo "https://${NODE_AUTH_TOKEN}:@github.com" > $HOME/.git-credentials
 # git checkout master
 
 # Setup conventional-github-releaser credentials
 # export CONVENTIONAL_GITHUB_RELEASER_TOKEN="$GITHUB_TOKEN"
 
 # Setup npm credentials
-echo "//npm.pkg.github.com/:_authToken=${NODE_AUTH_TOKEN}"  > $HOME/.npmrc
+echo ${NODE_AUTH_TOKEN}
+echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" > $HOME/.npmrc
